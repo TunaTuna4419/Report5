84a8db26fffd17c17659abe375ac0f246a80a237
Tsuha Shintaro <e235734@eve.u-ryukyu.ac.jp>
2023-11-29 13:17:36 +0900

    2nd commit step2

--- Report5.java
+++ Report5.java
@@ -1,6 +1,11 @@
 public class Report5{
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+          } catch (NullPointerException err) {
+            System.out.println("長さを確認できません。");
+            System.err.println(err.getMessage());
+          }
     }
 }
